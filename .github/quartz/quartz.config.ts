e08a9d617149059c7fe3c5482b73b133fd1e7afc
viv <116047994+itsvivianmill@users.noreply.github.com>
2025-10-21 15:33:20 -0700

    Update dark mode light color in quartz.config.ts

--- .github/quartz/quartz.config.ts
+++ .github/quartz/quartz.config.ts
@@ -38,7 +38,7 @@ const config: QuartzConfig = {
           highlight: "#E2E4CC"
         },
         darkMode: {
-          light: "#2C2825",
+          light: "#17100b",
           lightgray: "#585B4B",
           gray: "#5B6E36",
           darkgray: "#C5C6B7",
